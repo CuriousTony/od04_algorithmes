92e095158c17145fd3c6b8e42267f2da6913f632
CuriousTony <aszdorovtsev@yandex.ru>
2025-01-12 12:41:02 +0300

    Finished the binary search example.

---
+++ Binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(data, target):
+    start, end = 0, len(data) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if data[mid] == target:
+            return mid
+        elif data[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return None
+
+
+rand_list = [n for n in range(1, 21)]
+print(rand_list)
+print(binary_search(rand_list, 16))
